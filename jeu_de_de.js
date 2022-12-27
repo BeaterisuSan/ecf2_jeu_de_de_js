cbfce3607b79164cc087378723e73950366f3e56
BeaterisuSan <beatrice.arnaud8@wanadoo.fr>
2022-12-27 19:21:46 +0100

    Mise en page New Game Player One Player Two

--- jeu_de_de.js
+++ jeu_de_de.js
@@ -1,4 +1,20 @@
-let round = 0;
+const buttonNewGame = document.getElementById("BeginGame");
+const playerOne = 
+
+buttonPlay.addEventListener("click", function(){
+    beginGame();
+});
+
+function beginGame(){
+
+
+}
+
+
+
+
+
+/*let round = 0;
 
 document.getElementaryById("beginGame")
 .addEventListener("click", function(){
@@ -8,4 +24,4 @@ alert(round);
 
 function getRandomInt(max){
     return Math.floor(Math.random() *);
-}
+}*/
